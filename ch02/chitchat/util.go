998c67460aca1a4d2fa800a69535997da79e37cb
kiriyama-keisuke <kiriyama_keisuke@r.recruit.co.jp>
2020-12-29 20:00:02 +0900

    list 2.10

--- ch02/chitchat/util.go
+++ ch02/chitchat/util.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
+	"text/template"
 )
 
 func session(w http.ResponseWriter, r *http.Request) (sess data.Sesstion, err error) {
@@ -13,4 +15,14 @@ func session(w http.ResponseWriter, r *http.Request) (sess data.Sesstion, err er
 		}
 	}
 	return
+}
+
+func generateHTML(w http.ResponseWriter, data interface{}, fn ...string) {
+	var files []string
+	for _, file := range fn {
+		files = append(files, fmt.Sprintf("templates/%s.html", files))
+	}
+
+	templates := template.Must(template.ParseFiles(files...))
+	templates.ExecuteTemplate(writer, "layout", data)
 }
