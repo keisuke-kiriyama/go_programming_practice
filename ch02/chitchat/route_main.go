557fa9e64fbcb4700d084a884dd87acdfefcbdf9
keisuke-kiriyama <krkr.xm.rkr7x@gmail.com>
2020-12-30 14:59:27 +0900

    list 3.5

--- ch02/chitchat/route_main.go
+++ ch02/chitchat/route_main.go
@@ -9,17 +9,11 @@ func index(w http.ResponseWriter, r *http.Request) {
 	threads, err := data.threads()
 	if err == nil {
 		_, err := session(w, r)
-		public_templ_files := []string{"templates/layout.html",
-			"templates/public.navbar.html",
-			"templates/index.html"}
-		private_tmpl_files := []string{"templates/layout.html",
-			"templates/private.navbar.html",
-			"templates/index.html"}
 		var templates *template.Template
 		if err != nil {
-			templates = template.Must(template.ParseFiles(public_tmpl_files...))
+			generateHTML(writer, threads, "layout", "public.navbar", "index")
 		} else {
-			templates = template.Must(template.ParseFiles(private_tmpl_files...))
+			generateHTML(writer, threads, "layout", "private.navbar", "index")
 		}
 		templates.ExecuteTemplate(w, "layout", threads)
 	}
