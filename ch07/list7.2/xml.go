23c31c543ce905e3b16934cb12b2390ffccf95fe
keisuke-kiriyama <krkr.xm.rkr7x@gmail.com>
2021-01-01 19:17:37 +0900

    list7.6

--- ch07/list7.2/xml.go
+++ ch07/list7.2/xml.go
@@ -3,17 +3,18 @@ package main
 import (
 	"encoding/xml"
 	"fmt"
-	"io/ioutil"
+	"io"
 	"os"
 )
 
 // ずべての場所から参照可能でなければいけないため、大文字
 type Post struct {
-	XMLName xml.Name `xml:"post"`     // XML要素名自体を保存する
-	Id      string   `xml:"id, attr"` // attrがついているのは、XMLの属性
-	Content string   `xml:"content"`  // フィールド名と同じ名前のXML要素と紐付けられる
-	Author  Author   `xml:"author"`
-	Xml     string   `xml:",innerxml"` // XML要素から未処理のままで生のXMLを得る
+	XMLName  xml.Name  `xml:"post"`     // XML要素名自体を保存する
+	Id       string    `xml:"id, attr"` // attrがついているのは、XMLの属性
+	Content  string    `xml:"content"`  // フィールド名と同じ名前のXML要素と紐付けられる
+	Author   Author    `xml:"author"`
+	Xml      string    `xml:",innerxml"` // XML要素から未処理のままで生のXMLを得る
+	Comments []Comment `xml:"comments>comment"`
 }
 
 // データを表す構造体を定義する
@@ -22,19 +23,53 @@ type Author struct {
 	Name string `xml:",chardata"` // XML要素の文字データの保存
 }
 
+type Comment struct {
+	Id      string `xml:"id, attr"`
+	Content string `xml:"content"`
+	Author  Author `xml: "author"`
+}
+
 func main() {
 	xmlFile, err := os.Open("post.xml")
 	if err != nil {
 		fmt.Println("Erro opening XML file:", err)
 		return
 	}
-	defer xmlFile.Close()
-	xmlData, err := ioutil.ReadAll(xmlFile)
-	if err != nil {
-		fmt.Println("Error reading XML data:", err)
-	}
 
-	var post Post
-	xml.Unmarshal(xmlData, &post)
-	fmt.Println(post)
+	// 小さめのXMLの場合
+	// defer xmlFile.Close()
+	// xmlData, err := ioutil.ReadAll(xmlFile)
+	// if err != nil {
+	// 	fmt.Println("Error reading XML data:", err)
+	// }
+
+	// var post Post
+	// xml.Unmarshal(xmlData, &post)
+	// fmt.Println(post)
+
+	// 大きいXMLやストリームとして入力されるXMLの場合
+	// XMLからdecoderを生成
+	decoder := xml.NewDecoder(xmlFile)
+	// decoder内のXMLデータを順次処理
+	for {
+		// 各処理でdecoderからトークンを取得
+		t, err := decoder.Token()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			fmt.Println("Error decoding XML into tokens:", err)
+			return
+		}
+
+		// トークンの型をチェック
+		switch se := t.(type) {
+		case xml.StartElement: // XML要素の開始タグ
+			if se.Name.Local == "comment" {
+				var comment Comment
+				// XMLデータを構造体にデコード
+				decoder.DecodeElement(&comment, &se)
+			}
+		}
+	}
 }
